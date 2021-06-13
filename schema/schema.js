97e8540b7a055147792c8d9e455f5236f1a5f0db
Svetlana Shinkar <svetanek@gmail.com>
2021-06-13 00:22:41 -0400

    set up relations inc. bidirectional for schema

--- schema/schema.js
+++ schema/schema.js
@@ -5,18 +5,41 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
+  GraphQLList,
   GraphQLSchema
 } = graphql
 
 // const users = [{id: '25', firstName: 'Mark', age: 20}, {id: '2', firstName: 'Yesenia', age: 21}]
-
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    description: {type: GraphQLString},
+    users: {
+      type: new GraphQLList(UserType),
+      async resolve(parentValue, args) {
+        const {data} = await axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+        console.log(parentValue)
+        return data
+      }
+    }
+  })
+})
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: {type: GraphQLString},
     firstName: {type: GraphQLString},
-    age: {type: GraphQLInt}
-  }
+    age: {type: GraphQLInt},
+    company: {
+      type: CompanyType,
+      async resolve(parentValue, args) {
+        const {data} = await axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+        return data
+      }
+    }
+  })
 });
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -31,6 +54,14 @@ const RootQuery = new GraphQLObjectType({
       // return _.find(users, {id: args.id})
 
     }
+   },
+   company: {
+     type: CompanyType,
+     args: {id: {type: GraphQLString}},
+     async resolve(parentValue, args) {
+       const {data} = await axios.get(`http://localhost:3000/companies/${args.id}`)
+       return data
+     }
    }
   }
 })
